48b9f2264c4040cb28f49a79b24465680bae1cf8
Brian Paiement <bpaiement@MacBook-Air.local>
2026-09-16 16:15:13 -0400

    Hide *.pages.dev; keep the site on reformthenorth.com only

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,19 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  if (url.hostname === "www.reformthenorth.com") {
+  const host = url.hostname;
+
+  if (host.endsWith(".pages.dev")) {
+    return new Response(null, {
+      status: 404,
+      headers: { "x-robots-tag": "noindex, nofollow" },
+    });
+  }
+
+  if (host === "www.reformthenorth.com") {
     url.hostname = "reformthenorth.com";
     return Response.redirect(url.toString(), 301);
   }
+
   return context.next();
 }
+
